ea12343e38cabd1a44d23d88663865e9a17ca9fd
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2025-09-06 15:48:27 +0100

    install legal dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
+import 'react-native-reanimated';
+import "../global.css"
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import ClassOverviewScreen from "./screens/ClassOverviewScreen";
 import StudentDetailScreen from "./screens/StudentDetailScreen";
-import "../global.css"
-
 import type { RootStackParamList } from "./types/navigation";
+
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
